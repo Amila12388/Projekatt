14c7429bc02aa7e41e99ae140bd9cce7287f3ab9
Amila <amilav02@gmail.com>
2021-05-07 17:21:41 +0200

    Importing Project

---
+++ Shoppingg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shoppingg.Startup))]
+namespace Shoppingg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
